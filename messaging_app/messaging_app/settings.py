4d49eda60a6eee0fdfbbb69e19424939eb00098a
James <qjaymce37@gmail.com>
2025-09-27 20:07:04 +0000

    messaging_app

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -149,6 +149,8 @@ CORS_ALLOW_ALL_ORIGINS = True  # Only for development
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
