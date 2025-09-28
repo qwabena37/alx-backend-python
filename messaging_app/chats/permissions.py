b375ddbd019b086da39d300d3e3316471440cb81
James Appiah <159532433+qwabena37@users.noreply.github.com>
2025-09-28 12:40:39 +0000

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -31,6 +31,22 @@ class IsAuthenticatedCustom(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_authenticated)
+
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsOwnerOrReadOnly(BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    Others can only read (GET, HEAD, OPTIONS).
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed for any request
+        if request.method in SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the object
+        return obj.owner == request.user
     
     # chats/permissions.py
 from rest_framework import permissions
